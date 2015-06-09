1fa9ff8710f9420af9c5ce526f11fefee4f6bf76
Otavio Augusto <otavioaugustoes@gmail.com>
2015-06-09 14:59:25 -0300

    Angular post request

--- Shopping/WebContent/js/shopping.js
+++ Shopping/WebContent/js/shopping.js
@@ -1,4 +1,23 @@
 var shopping = angular.module('shopping', []);
-shopping.controller('userCadastreControl', function ($scope) {
- 
+shopping.controller('userCadastreControl', function ($scope,$http) {
+	$scope.user={};
+	
+	
+	
+	$scope.cadastrar= function(){
+		
+		$http( {method:'POST',
+			url:'cadastrarUser',
+			data: $.param($scope.user),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+		).success(function(data, status, headers, config) {
+		    console.log("sucess");
+		  }).error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+			  console.log("Erro");
+		  });
+		
+	
+	};
 });
