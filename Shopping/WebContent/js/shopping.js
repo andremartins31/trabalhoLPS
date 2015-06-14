49ce25b9bfd01aba5d3354fb46aed3f0139a4954
Otavio Augusto <otavioaugustoes@gmail.com>
2015-06-14 15:12:45 -0300

    Cadastro Serviço

--- Shopping/WebContent/js/shopping.js
+++ Shopping/WebContent/js/shopping.js
@@ -2,10 +2,15 @@ var shopping = angular.module('shopping', []);
 shopping.controller('userCadastreControl', function($scope, $http,$location) {
 	$scope.user = {};
 	$scope.produto = {};
+	$scope.servico={};
 	$scope.listProduct = [];
 	$scope.showList = true;
 	$scope.showDetail = false;
 	$scope.user = undefined;
+	$scope.passoUM =true;
+	$scope.telaProduct = false;
+	$scope.telaServico = false;
+	
 	function serializeData(data) {
 		// If this is not an object, defer to native stringification.
 		if (!angular.isObject(data)) {
@@ -121,5 +126,37 @@ shopping.controller('userCadastreControl', function($scope, $http,$location) {
 		}else{
 			window.location='login.html';
 		}
+	}
+	$scope.segundoPasso= function(opcao) {
+		$scope.passoUM = false;
+		switch (opcao) {
+		case 1:
+			$scope.telaProduct = true;
+			break;
+		case 2:
+			$scope.telaServico = true;
+		default:
+			break;
+		}
+		
+	}
+	
+	$scope.cadastrarServico = function() {
+		
+		$http({
+			method : 'POST',
+			url : 'cadastrarServico',
+			data : serializeData($scope.servico),
+			headers : {
+				'Content-Type' : 'application/x-www-form-urlencoded'
+			}
+		}).success(function(data, status, headers, config) {
+			alert("Serviço cadastrado com sucesso");
+		}).error(function(data, status, headers, config) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+			console.log("Erro");
+			alert("Erro" + status);
+		});
 	}
 });
